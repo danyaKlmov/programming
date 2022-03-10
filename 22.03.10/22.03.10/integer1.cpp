bab2bbd52bdef19ba08a5b82a4de368a6df40a9a
danyaKlmov <klimovdaneel@gmail.com>
2022-03-10 18:47:50 +0300

    1,6

---
+++ 22.03.10/22.03.10/integer1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main1() {
+	int l;
+	cin >> l;
+	int a;
+	a = l / 100;
+	cout << a;
+	return 0;
+}
---
+++ 22.03.10/22.03.10/integer6.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main() {
+	int a, b, c;
+	cin >> a;
+	b = a / 10;
+	c = a % 10;
+	cout << b << ' ' << c;
+	return 0;
+}
