a840f5f36e93da441807ef9f74592e8c7ac394aa
danyaKlmov <klimovdaneel@gmail.com>
2022-03-13 17:04:50 +0300

    2

--- 22.03.10/22.03.10/boolean29.cpp
+++ 22.03.10/22.03.10/boolean29.cpp
@@ -3,11 +3,11 @@
 
 using namespace std;
 
-int main() {
+int main10() {
 	int x, y, x1, y1, x2, y2;
 	cin >> x >> y >> x1 >> y1 >> x2 >> y2;
 	bool b = (x > x1) && (x < x2) && (y > y2) && (y < y1);
-	cout << b;
+	cout << b; 
 	return 0;
 
 }
---
+++ 22.03.10/22.03.10/integer2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main() {
+	int m;
+	cin >> m;
+	int a;
+	a = m / 1000;
+	cout << a;
+	return 0;
+}
