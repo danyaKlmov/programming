67c35084ec7cd27c01ec6d8030df2f3ef716e187
danyaKlmov <klimovdaneel@gmail.com>
2022-03-17 18:22:09 +0300

    14

---
+++ 13 03 22/13 03 22/if14.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+	int a, b, c, min, max;
+	cin >> a >> b >> c;
+	min = a;
+	max = b;
+	if ((b < a) && (b < c)) {
+		min = b;
+	}
+	if ((c < a) && (c < b)) {
+		min = c;
+	}
+	if ((a > b) && (a > c)) {
+		max = a;
+	}
+	if ((c > a) && (c > b)) {
+		max = c;
+	}
+	cout << min << ' ' << max;
+	return 0;
+}
