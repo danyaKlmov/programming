6d9b8faf5d178a9b55e378581125648ec4158195
danyaKlmov <klimovdaneel@gmail.com>
2022-03-17 17:34:06 +0300

    3

--- 13 03 22/13 03 22/if1.cpp
+++ 13 03 22/13 03 22/if1.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 using namespace std;
-int main() {
+int main9() {
 	int a;
 	cin >> a;
 	if (a > 0) {
---
+++ 13 03 22/13 03 22/if5.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+	int a, b, c, k1, k2;
+	k1 = 0;
+	k2 = 0;
+	cin >> a >> b >> c;
+	if (a > 0) {
+		k1 = k1 + 1;
+	}
+	if (b > 0) {
+		k1 = k1 + 1;
+	}
+	if (c > 0) {
+		k1 = k1 + 1;
+	}
+	if (a < 0) {
+		k2 = k2 + 1;
+	}
+	if (b < 0) {
+		k2 = k2 + 1;
+	}
+	if (c < 0) {
+		k2 = k2 + 1;
+	}
+	cout << k1 << ' ' << k2;
+	return 0;
+}
