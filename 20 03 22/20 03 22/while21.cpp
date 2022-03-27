bc15db5bd5ec7580949b089597c7ae8678a6389d
danyaKlmov <klimovdaneel@gmail.com>
2022-03-27 17:56:04 +0300

    21

--- 20 03 22/20 03 22/while21.cpp
+++ 20 03 22/20 03 22/while21.cpp
@@ -1,13 +1,13 @@
 #include<iostream>
 
 using namespace std;
-int main19() {
+int main() {
 	int n;
 	cin >> n;
-	while ((n > 0) & (( n % 10) != 2)) {
+	while ((n > 0) & ((n % 10) % 2 == 0)) {
 		n /= 10;
 	}
-	if ((n > 0) && ( n % 10) == 2) {
+	if ((n % 10) % 2 != 0) {
 		cout << "true";
 	}
 	else {
