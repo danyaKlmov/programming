add18c21a34fb308402e9956151ddf0887436f4f
danyaKlmov <klimovdaneel@gmail.com>
2022-03-27 19:32:00 +0300

    13

--- 27 03 22/27 03 22/for10.cpp
+++ 27 03 22/27 03 22/for10.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int main() {
+int main7() {
 	int n;
 	cin >> n;
 	double s = 0;
---
+++ 27 03 22/27 03 22/for13.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int n, sign = 1;
+	cin >> n;
+	double a = 0;
+	for (int i = 1; i <= n; i++) {
+		a += sign * (1.0 + i / 10.0);
+		sign *= -1;
+		cout << a << ' ';
+	}
+	return 0;
+}
