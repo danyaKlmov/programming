6e380d0843d4cdbf23984a8576e1ddc76c787370
danyaKlmov <klimovdaneel@gmail.com>
2022-04-07 19:21:50 +0300

    1

---
+++ practicanumber1/practicanumber1/№1.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+struct Transport {
+	string name;
+	string surname;
+	string patronymic;
+	int luggage;
+	double weight;
+};
+
+int main() {
+	int n;
+	cout << "vvedite kol-vo passazhirov:\n";
+	cin >> n;
+	Transport* a = new Transport[n];
+	for (int i = 0; i < n; ++i) {
+		cout << "vvedite informaciu o " << i + 1 << "-m passazhire:\n";
+		cin >> a[i].name >> a[i].surname >> a[i].patronymic >> a[i].luggage >> a[i].weight;
+	}
+	int k = 0;
+	for (int i = 0; i < n; ++i) {
+		if (a[i].luggage > 3) {
+			k++;
+		}
+	}
+	cout << "kol-vo passazhirov u kotorix > 3 veshei: " << k;
+	delete[] a;
+	return 0;
+}
